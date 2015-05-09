b41209ac13ff8591f4002e712e685784a9951353
1000miles <1000miles@users.noreply.github.com>
2015-05-09 10:45:06 +0200

    add two solutions of Fizz Buzz

--- game.rb
+++ game.rb
@@ -6,12 +6,40 @@
 # divisible by 3 and 5 output 'Fizz Buzz'.
 # In any other case output the number.
 
-number = 1
 
-while number <= 50 do
-   puts number
-   number = number + 1
-   if number % 5 == 0
-     puts 'Fizz'
-   end
+
+def solution1
+	(1..50).each do |i|
+	number = String.new
+	number << "Rubies " if i % 3 == 0
+	number << "On The Block" if i % 5 == 0
+	number << i.to_s if number.empty?
+	puts number
+	end
 end
+
+solution1
+
+puts "----------------"
+
+def solution2
+
+number = 0
+
+	while number < 50
+		number += 1
+
+		if ((number % 3 == 0) && (number % 5 == 0))
+			puts 'Rubies On Heaven ' 
+		elsif number % 3 == 0
+		 	puts 'Rubies '
+		elsif number % 5 == 0
+			puts "On Heaven " 
+		else 
+			puts number
+		end
+	end
+end
+
+solution2
+
